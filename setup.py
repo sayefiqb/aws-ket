0cdf01cdb7e977d28467ee3135f28d0eced473d0
Sayef Iqbal <34716299+sayefiqb@users.noreply.github.com>
2023-03-23 20:40:48 -0400

    updating setup and pyprojects for pypi release (#36)
    Co-authored-by: sayef <sayef.iqbal@veea.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+VERSION = "0.1.1"
+setup(
+    name             = 'aws-ket',
+    author           = 'Sayef Iqbal',
+    author_email     = 's2400@columbia.edu',
+    maintainer       = 'Sayef Iqbal',
+    url              = 'https://github.com/sayefiqb/aws-ket',
+    description      = 'Utility tool to encrypt data using AWS KMS and store it in preferred backend.',
+    version          = VERSION
+)
