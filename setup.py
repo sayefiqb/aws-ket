8c8372d362e8cbdfe232f1a12218b50881d50a17
sayef <sayef.iqbal@veea.com>
2023-04-13 20:02:18 -0400

    fixing issue with setup.py file

--- setup.py
+++ setup.py
@@ -11,14 +11,6 @@ setup(
     version          = VERSION,
     include_package_data= True,
     entry_points = {
-        'console_scripts': ['awsket=awsket.ket:create_kms_alias',
-                            'awsket=awsket.ket:create_kms_key',
-                            'awsket=awsket.ket:create_s3_bucket',
-                            'awsket=awsket.ket:check_alias',
-                            'awsket=awsket.ket:get_iam_user',
-                            'awsket=awsket.ket:push_to_s3',
-                            'awsket=awsket.ket:encrypt_file',
-                            'awsket=awsket.ket:encrypt_text',
-                            'awsket=awsket.ket:decrypt_text']
+        'console_scripts': ['awsket=awsket.ket']
     }
 )
